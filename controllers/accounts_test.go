b6a7c1194e05b774196f18c501095033f582a3c2
Nelson Haraguchi <nelsonmhjr@gmail.com>
2020-05-06 06:24:13 -0300

    Refactor

--- controllers/accounts_test.go
+++ controllers/accounts_test.go
@@ -24,25 +24,44 @@ var _ = Describe("Accounts", func() {
 	})
 
 	Describe("FindAccount GET /accounts/:accountId", func() {
-		Context("Without a matching BankAccount with the matchin ID", func() {
-			It("Should render a Bad Request", func() {
-				resp, err := http.Get(fmt.Sprintf("%s/accounts/1", Ts.URL))
-				Expect(err).To(BeNil())
+		var request = func(id uint) *http.Response {
+			resp, err := http.Get(fmt.Sprintf("%s/accounts/%d", Ts.URL, id))
+			Expect(err).To(BeNil())
+			return resp
+		}
 
+		var requestBody = func(resp *http.Response) string {
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			Expect(err).To(BeNil())
+			return string(bodyBytes)
+		}
+
+		Context("without a matching BankAccount with the matchin ID", func() {
+			It("should render a Bad Request", func() {
+				resp := request(1)
 				Expect(resp.StatusCode).To(Equal(400))
 			})
 		})
 
-		Context("With a matching BankAccount ID", func() {
-			It("Should render JSON with id and document_number", func() {
+		Context("with a matching BankAccount ID", func() {
+			It("should render JSON with id and document_number", func() {
 				bankAcc := models.BankAccount{DocumentNumber: "123456"}
 				Db.Create(&bankAcc)
-				resp, err := http.Get(fmt.Sprintf("%s/accounts/%d", Ts.URL, bankAcc.ID))
-				Expect(err).To(BeNil())
-				bodyBytes, err := ioutil.ReadAll(resp.Body)
-				Expect(err).To(BeNil())
-				bodyString := string(bodyBytes)
-				Expect(bodyString).To(MatchJSON(fmt.Sprintf(`{"document_number":"%s","account_id":%d}`, bankAcc.DocumentNumber, bankAcc.ID)))
+				resp := request(bankAcc.ID)
+				bodyString := requestBody(resp)
+				Expect(bodyString).To(
+					MatchJSON(
+						fmt.Sprintf(`{"document_number":"%s","account_id":%d}`,
+							bankAcc.DocumentNumber, bankAcc.ID)))
+			})
+		})
+	})
+
+	Describe("CreateAccounts POST /accounts", func() {
+		Context("without a json body", func() {
+			It("should render a UnprocessableEntity Status Code", func() {
+				// resp, err := http.Get(fmt.Sprintf("%s/accounts/", Ts.URL))
+
 			})
 		})
 	})
