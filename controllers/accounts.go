51ffdea117b53006455a8b64de7e0f374f89b702
Nelson Haraguchi <nelsonmhjr@gmail.com>
2020-05-05 22:47:27 -0300

    Create Accounts Working!

--- controllers/accounts.go
+++ controllers/accounts.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/nelsonmhjr/bank_service/models"
@@ -9,10 +10,27 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+type accountToCreate struct {
+	DocumentNumber string `form:"document_number" json:"document_number" binding:"required"`
+}
+
 // POST /accounts
 // CreateAccounts is used to create an BankAccount with the given data
 func CreateAccounts(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{"status": "created"})
+	db := c.MustGet("db").(*gorm.DB)
+	var data accountToCreate
+	err := c.ShouldBind(&data)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "Invalid Data"})
+		return
+	}
+	bankAccount := models.BankAccount{DocumentNumber: data.DocumentNumber}
+	db.Create(&bankAccount)
+
+	c.JSON(http.StatusCreated, gin.H{"status": "created", "account": gin.H{
+		"account_id":      bankAccount.ID,
+		"document_number": bankAccount.DocumentNumber}})
 }
 
 // GET /accounts/:accountId
@@ -22,7 +40,8 @@ func FindAccount(c *gin.Context) {
 
 	// Get model if exist
 	var bankAccount models.BankAccount
-	if err := db.Where("id = ?", c.Param("accountId")).First(&bankAccount).Error; err != nil {
+	err := db.Where("id = ?", c.Param("accountId")).First(&bankAccount).Error
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
