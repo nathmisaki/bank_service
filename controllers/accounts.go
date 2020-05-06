931f17b371aee82de25abb44ec448d392505d16d
Nelson Haraguchi <nelsonmhjr@gmail.com>
2020-05-05 21:02:34 -0300

    Working find an Account

--- controllers/accounts.go
+++ controllers/accounts.go
@@ -3,10 +3,31 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/nelsonmhjr/bank_service/models"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
-// CreateAccounts is used to create an Account with the given data
+// POST /accounts
+// CreateAccounts is used to create an BankAccount with the given data
 func CreateAccounts(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"status": "created"})
+}
+
+// GET /accounts/:accountId
+// FindAccount is used to fetch information about an BankAccount
+func FindAccount(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	// Get model if exist
+	var bankAccount models.BankAccount
+	if err := db.Where("id = ?", c.Param("accountId")).First(&bankAccount).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"account_id":      bankAccount.ID,
+		"document_number": bankAccount.DocumentNumber})
 }
--- main.go
+++ main.go
@@ -9,52 +9,8 @@ import (
 	"github.com/nelsonmhjr/bank_service/models"
 )
 
-var db = make(map[string]string)
-
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
-
-	r.POST("/accounts", controllers.CreateAccounts)
-
-	return r
-}
-
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
+	r := gin.Default()
 	db := models.SetupModels()
 
 	// Provide db variable to controllers
@@ -63,5 +19,14 @@ func main() {
 		c.Next()
 	})
 
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/accounts/:accountId", controllers.FindAccount)
+	r.POST("/accounts", controllers.CreateAccounts)
+
+	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
